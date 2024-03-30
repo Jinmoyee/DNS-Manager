d5e2f3c5f4fd04dd030072ee164888fce495b688
Jinmoyee Thakuria <jinmoyee99@gmail.com>
2024-03-30 11:50:12 +0530

    humm

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,6 @@ mongoose.connect(process.env.MONGO).then(() => {
   console.log("Connected to mongoDB");
 });
 
-app.listen(1000, (req, res) => {
-  console.log("Server is running at port 1000");
+app.listen(3000, (req, res) => {
+  console.log("Server is running at port 3000");
 });
