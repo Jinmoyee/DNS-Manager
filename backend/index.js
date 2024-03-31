5c6ea8714af5c33730aa474190a95a42e773cc71
Jinmoyee Thakuria <jinmoyee99@gmail.com>
2024-03-31 19:22:06 +0530

    hum

--- backend/index.js
+++ backend/index.js
@@ -28,8 +28,8 @@ app.use("/", Edit);
 app.use("/", Data);
 app.use("/", Delete);
 
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
